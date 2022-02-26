4404b4e8a3ea7f65362af38144b545d8ec975fe6
Khuong HM <khuonghm@physcode.com>
2022-02-27 00:05:04 +0700

    Update layout desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-$('.banner-slider').slick({
+$('.banner-slider, .customer-slider').slick({
     infinite: true,
     slidesToShow: 1,
     centerMode: true,
@@ -8,6 +8,20 @@ $('.banner-slider').slick({
     swipeToSlide: true,
     cssEase: 'linear',
     centerPadding: 0,
+    arrows: true,
+    prevArrow: '<div class="slick-prev"><i class="las la-angle-left"></i></div>',
+    nextArrow: '<div class="slick-next"><i class="las la-angle-right"></i></div>',
+});
+$('.category-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    centerMode: true,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    cssEase: 'linear',
+    centerPadding: '0',
     arrows: true,
     prevArrow: '<div class="slick-prev"><i class="las la-angle-left"></i></div>',
     nextArrow: '<div class="slick-next"><i class="las la-angle-right"></i></div>',
